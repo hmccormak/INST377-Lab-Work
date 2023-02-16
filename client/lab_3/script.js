2f704d9cc7ac0f15e5154bd736bc29f6fc76676a
hmccormak <82132780+hmccormak@users.noreply.github.com>
2023-02-16 15:27:07 -0500

    shrek complete

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -24,8 +24,15 @@ function updateSlidePosition() {
   // and remove the 'visible' class from each classList
   // then add a class 'hidden' to all of them
 
+  slidesArray.forEach(slide => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  })
+
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
+
+  slides[slidePosition].classList.add('visible');
 }
 
 function moveToNextSlide() {
@@ -35,6 +42,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if(slidePosition === totalSlides - 1){
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -45,6 +57,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+ if(slidePosition === 0){
+  slidePosition = totalSlides - 1;
+ } else {
+  slidePosition -= 1;
+ }
   updateSlidePosition();
 }
 
@@ -60,3 +77,9 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+
+document.querySelector('.prev')
+  .addEventListener('click', () => {
+    console.log('clicked prev');
+    moveToPrevSlide();
+  })
